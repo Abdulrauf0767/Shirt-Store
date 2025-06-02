70e25b8a68be543c3b9ce8c92a831f765b689e0e
Abdulrauf0767 <araof1803@mail.com>
2025-06-02 14:32:39 +0500

    set duration of transition

--- src/components/AnimatedPageWrapper.jsx
+++ src/components/AnimatedPageWrapper.jsx
@@ -5,7 +5,7 @@ const animations = {
   initial: { opacity: 0, y: 20 },
   animate: { opacity: 1, y: 0 },
   exit: { opacity: 0, y: -20 },
-  transition: { duration: 0.4 },
+  transition: { duration: 0.2 },
 };
 
 const AnimatedPageWrapper = ({ children }) => {
